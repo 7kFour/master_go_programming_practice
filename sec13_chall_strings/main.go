5d218b7ad53ce4be85794d1590378e68e6d4d7c5
7kFour <48108297+7kFour@users.noreply.github.com>
2021-10-17 22:07:53 -0500

    finish sec 11 chall, add sec 13 chall

--- sec13_chall_strings/main.go
+++ sec13_chall_strings/main.go
@@ -1,1 +1,20 @@
 package main
+
+import "fmt"
+
+func main () {
+	// ex 1
+	var name = "Anthony"
+	country := "United States"
+	p := fmt.Println
+	fmt.Printf("My Name: %v\nCountry: %v\n", name, country)
+	p(`He says: "Hello"`)
+	p(`C:\Users\a.txt`)
+
+	// ex 2
+	// a with caron
+	r := 'ǎ'
+	fmt.Printf("r is type: %T\n", r)
+	s1, s2 := "ma", "m"
+	p(s1 + s2 + string(r))
+}
